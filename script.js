fd30bdd51848d54e91f19683287dfc30d0f06a62
Juliana <jumiranda0501@gmail.com>
2022-12-13 14:31:06 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// Date to count down to
+const countDownDate = new Date("Dec 17, 2022 10:00:00").getTime();
+
+// Update the count down every 1 second
+const interval = setInterval(countDown, 1000);
+
+function countDown() {
+
+    // Get today's date and time
+    const now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    const distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    document.getElementById("days").textContent = days;
+    document.getElementById("hours").textContent = hours;
+    document.getElementById("minutes").textContent = minutes;
+    document.getElementById("seconds").textContent = seconds;
+
+    if (distance < 0) {
+        clearInterval(interval);
+    }
+
+}
